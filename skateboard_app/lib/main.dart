4fda3ad6b249e7b30543061e02269bd8cf44dc50
oscarrep <oscar.bcn.95@gmail.com>
2020-11-07 21:04:58 +0100

    custom color

--- skateboard_app/lib/main.dart
+++ skateboard_app/lib/main.dart
@@ -12,7 +12,7 @@ class SkateboardApp extends StatelessWidget {
     return MaterialApp(
       //title: 'Skateboard App',
       theme: ThemeData(
-        primarySwatch: Colors.customPurple,
+        primarySwatch: customPurple,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: BoardPage(title: 'Skateboard App Home Page'),
@@ -104,3 +104,25 @@ class _BoardPageState extends State<BoardPage> {
     );
   }
 }
+
+
+
+
+
+MaterialColor customPurple = MaterialColor(
+    _customPurplePrimaryValue,
+    //0xFF9F69c7, color);
+    <int,Color>{
+     50: Color(0xFFfaf7fc),
+      100: Color(0xFFf5f0f9),
+      200: Color(0xFFece1f4),
+      300: Color(0xFFe2d2ee),
+      400: Color(0xFFd9c3e9),
+      500: Color(_customPurplePrimaryValue),
+      600: Color(0xFFc5a5dd),
+      700: Color(0xFFbc96d8),
+      800: Color(0xFFb287d2),
+      900: Color(0xFFa878cc),
+    },
+  );
+int _customPurplePrimaryValue = 0xFF9F69C7;
