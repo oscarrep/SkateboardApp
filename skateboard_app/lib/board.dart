592567f89f8effc7e726fb0a8018d072d97c5672
oscarrep <oscar.bcn.95@gmail.com>
2020-11-08 01:37:15 +0100

    base app structure

---
+++ skateboard_app/lib/board.dart
@@ -0,0 +1,33 @@
+class Board
+{
+  String boardPNG, itemName, description, sold, related, relatedPNG, totalPrice, money;
+  double price;  
+
+Board
+(
+this.boardPNG,
+this.itemName,
+this.description,
+this.sold,
+this.related,
+this.relatedPNG,
+this.totalPrice,
+this.money,
+this.price
+);
+
+}
+
+final board = Board(
+  'assets/skate.png',
+  'Rip Curl Skateboard',
+  'Wooden longboard in Best quality for '
+    'skateboard lovers we have wide range '
+    'skateboards.',
+  '15 sold in last 24 hours',
+  'Related products',
+  'assets/skate.png',
+  'Total Price',
+  '€',
+  75.00,
+);
--- skateboard_app/lib/main.dart
+++ skateboard_app/lib/main.dart
@@ -1,4 +1,4 @@
-//import 'package:skateboard_app/board.dart';
+import 'package:skateboard_app/board.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,101 +10,181 @@ class SkateboardApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      //title: 'Skateboard App',
       theme: ThemeData(
         primarySwatch: customPurple,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: BoardPage(title: 'Skateboard App Home Page'),
+      home: BoardPage(board),
     );
   }
 }
 
-class BoardPage extends StatefulWidget {
-  BoardPage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class BoardPage extends StatelessWidget {
+  final Board board;
+  BoardPage(this.board);
 
   @override
-  _BoardPageState createState() => _BoardPageState();
+  Widget build(BuildContext context) {
+    final divider = Divider(
+      color: Color.fromARGB(255, 0, 0, 0),
+      thickness: 0,
+      height: 0,
+    );
+
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(flex: 6, child: _Header(board: board)),
+          divider,
+          Expanded(flex: 2, child: _Middle(board: board)),
+          divider,
+          Expanded(flex: 2, child: _Bottom(board)),
+        ],
+      ),
+    );
+  }
 }
 
-class _BoardPageState extends State<BoardPage> {
-  int _counter = 0;
+// --------------------------------------------------------------- HEADER -------------------------------
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class _Header extends StatelessWidget {
+  const _Header({
+    Key key,
+    @required this.board,
+  }) : super(key: key);
+
+  final Board board;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          AspectRatio(
+            aspectRatio: 2,
+            child: Image.asset(
+              board.boardPNG,
+            ),
+          ),
+          Expanded(
+          child: Center (
+              child: Text(
+                board.itemName,
+                style: TextStyle(
+                fontSize: 20,
+            ),
+          ),
+          ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(20),
+              child: Text(
+                board.description,
+                style: TextStyle(
+                  fontSize: 13,
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// --------------------------------------------------------------- MIDDLE -------------------------------
+
+class _Middle extends StatelessWidget {
+  const _Middle({
+    Key key,
+    @required this.board,
+  }) : super(key: key);
+
+  final Board board;
+
+  Widget _text(String text) {
+    final TextStyle infoStyle = TextStyle(
+      fontSize: 12,
+      color: Colors.grey,
+    );
+    return Text(text, style: infoStyle);
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the BoardPage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Expanded(
+          child: Padding (
+            padding: EdgeInsets.fromLTRB(0,5,200,0),
+              child: Text(
+                board.sold,
+                style: TextStyle(
+                fontSize: 15,
+                color: Colors.red,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+          ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(20),
+              child: Text(
+                board.description,
+                style: TextStyle(
+                  fontSize: 13,
+                  color: Colors.grey,
+                ),
+              ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
 
+// --------------------------------------------------------------- BOTTOM -------------------------------
+
+class _Bottom extends StatelessWidget {
+  final Board board;
+  _Bottom(this.board);
+
+  _BottomRow(String label, String content) => TableRow(
+        children: [
+          Text(label),
+          SizedBox(width: 16),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 16.0),
+            child: Text(content),
+          ),
+        ],
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scrollbar(
+      child: ListView(
+        children: [
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Table(
+              columnWidths: {
+                0: IntrinsicColumnWidth(),
+                1: IntrinsicColumnWidth(),
+              },
+              
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
 
 
 
